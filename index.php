33c6c21cc508987c751b38f284934a059deef820
Phucpnt <phuc@phucpnt.com>
2014-08-06 12:00:38 +0700

    Support auto login

--- index.php
+++ index.php
@@ -1,9 +1,44 @@
 <?php
 
 function adminer_object() {
+
+  $server   = $_SERVER;
+  $hostname = $server['SERVER_NAME'];
+
   // required to run any plugin
   include_once "./plugins/plugin.php";
 
+  class AdminerExtend extends AdminerPlugin
+  {
+
+    protected $hostname = 'localhost.com';
+
+    public function setHostname($hostname) {
+      $this->hostname = $hostname;
+    }
+
+    public static function isDevHost($hostname) {
+      return (preg_match('/localhost/i', $hostname));
+    }
+
+    public function autoLogin($username) {
+      if (isset($_REQUEST['username'])) {
+        return;
+      }
+      if (self::isDevHost($this->hostname)) {
+        header('Location: ' . $_SERVER['SCRIPT_NAME'] . '?username=' . $username);
+        exit();
+      }
+    }
+
+    public function credentials() {
+      if (self::isDevHost($this->hostname)) {
+        return array('127.0.0.1', 'admin', 'abc123');
+      }
+      return array();
+    }
+
+  }
   // autoloader
   foreach (glob("plugins/*.php") as $filename) {
     include_once "./$filename";
@@ -13,16 +48,20 @@ function adminer_object() {
     // specify enabled plugins here
     new AdminerTablesFilter,
     new AdminerJsonColumn,
-    new AdminerSchemaLog,
   );
 
-  /* It is possible to combine customization and plugins:
-    class AdminerCustomization extends AdminerPlugin {
-    }
-    return new AdminerCustomization($plugins);
-   */
+  if (AdminerExtend::isDevHost($hostname)) {
+    $plugins[] = new AdminerSchemaLog;
+  }
 
-  return new AdminerPlugin($plugins);
+  /**
+   * myadminer customization including plugins
+   */
+  $myAdminer = new AdminerExtend($plugins);
+  $myAdminer->setHostname($hostname);
+  $myAdminer->autoLogin('admin');
+
+  return $myAdminer;
 }
 
 // include original Adminer or Adminer Editor
