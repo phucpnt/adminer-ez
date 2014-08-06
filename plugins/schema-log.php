99c99b38e0fd7c18947f3905ee5084176504598c
Phucpnt <phuc@phucpnt.com>
2014-08-06 11:22:43 +0700

    Add plugin supporting for loging table schema change

---
+++ plugins/schema-log.php
@@ -0,0 +1,42 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+class AdminerSchemaLog
+{
+
+  protected $dir = 'schema-log';
+
+  public function __construct() {
+    if (!is_dir($this->dir)) {
+      mkdir('schema-log', 0755, true);
+    }
+  }
+
+  public function messageQuery($query, $time) {
+    $isMatched = preg_match('/(alter|create)\s+table\s+/i', $query);
+    if (!$isMatched) {
+      return;
+    }
+    if ($this->filename == "") {
+      $adminer        = adminer();
+      $this->filename = implode(".", array(
+        $adminer->database(),
+        date('Y-m-d'),
+        'sql'
+      )); // no database goes to ".sql" to avoid collisions
+    }
+    $fp = fopen($this->dir . '/' . $this->filename, "a");
+    flock($fp, LOCK_EX);
+    fwrite($fp, '/* ========= ' . date('Y-m-d H:i:s') . ' =========*/' . "\n");
+    fwrite($fp, $query);
+    fwrite($fp, "\n\n");
+    flock($fp, LOCK_UN);
+    fclose($fp);
+  }
+
+}
