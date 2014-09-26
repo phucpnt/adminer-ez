d56ed7c5757ca1fc927f68143f9b9a69374b0538
Phuc PNT <phuc@phucpnt.com>
2014-09-27 01:52:35 +0700

    Filter on the tree view of table list + Apply the css for UI. + Remove unused files

--- js/main/tables-tree.js
+++ js/main/tables-tree.js
@@ -7,11 +7,73 @@
 
 (function () {
 
-	define(['jquery', 'vendors/jstree.min'], function ($) {
+	define(['jquery', 'vendors/tree.jquery'], function ($) {
+		/**
+		 * @type {Array}
+		 */
+		var data = ezTree;
+		var copied = JSON.stringify(ezTree);
 		var Module = function (el) {
 			var $el = $(el);
-			$el.find('.ez-tree-view').jstree().jstree('open_all');
-			;
+			var $tree = $el.find('.ez-tree-view');
+			$tree.tree({
+				data: data,
+				autoOpen: true,
+				selectable: false,
+				useContextMenu: false,
+				onCreateLi: markItUp
+			});
+			var $search = $el.find('.ez-table-search');
+			var tid = false;
+			$search.on('keyup', function () {
+				clearTimeout(tid);
+				console.log($search.val());
+				tid = setTimeout(function () {
+					$tree.tree('loadData', filterIt($search.val(), JSON.parse(copied)));
+				}, 100);
+			});
+		};
+		var filterIt = function (search, data) {
+			var filteredData = [];
+			if (search === '') {
+				return data;
+			}
+			$.each(data, function (index, group) {
+				if (group.table_name && group.table_name.indexOf(search) >= 0) {
+					filteredData.push(group);
+				}
+				else if (!group.children) {
+					return;
+				}
+				var filteredTabs = [];
+				$.each(group.children, function (index, table) {
+					if (table.table_name.indexOf(search) >= 0) {
+						filteredTabs.push(table);
+					}
+				});
+				if (filteredTabs.length) {
+					group.children = filteredTabs;
+					filteredData.push(group);
+				}
+			});
+			return filteredData;
+		};
+		var markItUp = function (table, $li) {
+			if (!table.table_name) {
+				return;
+			}
+			var select = document.createElement('a');
+			select.text = 'Select';
+			select.href = table.url_select;
+
+			var structure = document.createElement('a');
+			structure.text = table.table_name;
+			structure.href = table.url_structure;
+
+			$li.find('.jqtree-title')
+							.before(select)
+							.empty().append(structure)
+							;
 		};
 		return Module;
 	});
--- plugins/ez-tables-tree.php
+++ plugins/ez-tables-tree.php
@@ -10,8 +10,39 @@ class AdminerEzTablesTree
 {
 
 	function rowRender($table) {
-		echo '<span class="table-row"><a href="' . h(ME) . 'select=' . urlencode($table) . '"' . bold($_GET["select"] == $table) . ">" . lang('select') . "</a> ";
-		echo '<a href="' . h(ME) . 'table=' . urlencode($table) . '"' . bold($_GET["table"] == $table) . ">" . h($table) . "</a><br></span>\n";
+		echo '<span class="table-row">'
+		. '<a href="' . h(ME) . 'select=' . urlencode($table) . '"' . bold($_GET["select"] == $table, 'action') . ">" . lang('select') . "</a> ";
+		echo ' <a href="' . h(ME) . 'table=' . urlencode($table) . '"' . bold($_GET["table"] == $table, 'table-name') . ">" . h($table) . "</a><br>"
+		. "</span>\n";
+	}
+
+	function mapItems($tabs) {
+		$mapped = array();
+		foreach ($tabs as $tab) {
+			$mapped[] = array(
+				'label'				 => $tab,
+				'table_name'	 => $tab,
+				'url_select'	 => (ME) . 'select=' . urlencode($tab),
+				'url_structure' => (ME) . 'table=' . urlencode($tab)
+			);
+		}
+		return $mapped;
+	}
+
+	function optimize($morethan, $indentedGroup) {
+		$tree = array();
+		foreach ($indentedGroup as $prefix => $tables) {
+			if (count($tables) <= $morethan) {
+				$tree = array_merge($tree, $this->mapItems($tables));
+			} else {
+				$subTree = array(
+					'label'		 => $prefix,
+					'children' => $this->mapItems($tables),
+				);
+				$tree[]	 = $subTree;
+			}
+		}
+		return $tree;
 	}
 
 	function tablesPrint($tables) {
@@ -23,40 +54,15 @@ class AdminerEzTablesTree
 			}
 			$indent1st[$prefix][] = $table;
 		}
-		$moreThan = 2;
+		$moreThan	 = 2;
+		$tree			 = $this->optimize($moreThan, $indent1st);
 		?>
 		<div class="ez-tables-tree" data-component="main/tables-tree">
-			<p><input type="text" placeholder="search" class="ez-table-search"/></p>
+			<p><input type="text" placeholder="search table..." class="ez-table-search"/></p>
 			<div class="ez-tree-view">
-				<ul>
-					<?php
-					foreach ($indent1st as $prefix => $tabs):
-						if (count($tabs) > $moreThan):
-							?>
-							<li><?php echo $prefix ?>
-								<ul>
-									<?php
-									foreach ($tabs as $table):
-										?>
-										<li> <?php $this->rowRender($table); ?> </li>
-										<?php
-									endforeach;
-									?>
-								</ul>
-							</li>
-							<?php
-						else:
-							foreach ($tabs as $tab):
-								?>
-								<li> <?php $this->rowRender($tab); ?> </li>
-								<?php
-							endforeach;
-						endif;
-					endforeach;
-					?>
-				</ul>
 			</div>
 		</div>
+		<script>var ezTree = <?php echo json_encode($tree) ?>;</script>
 		<?php
 		return true;
 	}
